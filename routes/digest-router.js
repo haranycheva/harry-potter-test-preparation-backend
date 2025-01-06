3f5ec7cacec6b4cfb7fff1a626c24053d328f6d5
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-01-06 20:11:10 +0200

    Update digest-router.js

--- routes/digest-router.js
+++ routes/digest-router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import digestControllers from "../controllers/digest-controllers.js";
+import { authorization } from "../middleware/authorization.js";
 
 const digestRouter = express.Router();
 
@@ -7,21 +8,25 @@ digestRouter.get("/", digestControllers.getAllTopics);
 
 digestRouter.get(
   "/:id",
+  authorization,
   digestControllers.getTopicById
 );
 
 digestRouter.get(
   "/task/:id",
+  authorization,
   digestControllers.getTaskById
 );
 
 digestRouter.get(
   "/tasks/:topicId",
+  authorization,
   digestControllers.getTopicTasksInformation
 );
 
 digestRouter.get(
   "/test/:id",
+  authorization,
   digestControllers.getTestTasks
 );
 
