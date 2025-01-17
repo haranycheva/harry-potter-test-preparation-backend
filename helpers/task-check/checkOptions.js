33dd9bc04309b929a446d4ffecd0c7ecaa4d12a2
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-01-17 23:14:33 +0200

    Update checkOptions.js

--- helpers/task-check/checkOptions.js
+++ helpers/task-check/checkOptions.js
@@ -8,6 +8,9 @@ const checkOptions = async (task) => {
     throw HttpError(404, `Can not find task with id: ${_id}`);
   }
   const correctAnsw = await Option.findOne({task: _id, isCorrect: true})
+  if (!correctAnsw) {
+    throw HttpError(404, `No correct answer found in task ${_id}`);
+  }
   if(String(correctAnsw._id) === selectedOptionId){
     return {score: digestTask.possibleScore, userAnswer: {selectedOptionId, isCorrect: true},}
   }
